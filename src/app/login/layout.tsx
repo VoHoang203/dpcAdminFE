4320bb269445f9bd6303f004983d35708f51526d
VoHoang203 <davidtheshadowvo13@gmail.com>
2026-02-10 15:40:22 +0700

    no3

--- src/app/login/layout.tsx
+++
@@ -1,7 +0,0 @@
-export default function LoginLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
-  return <section className="min-h-screen p-6">{children}</section>;
-}
--- src/app/workspace/account-management/layout.tsx
+++
@@ -1,7 +0,0 @@
-export default function AccountManagementLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
-  return <section className="min-h-screen p-6">{children}</section>;
-}
